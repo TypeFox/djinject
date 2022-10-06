63902a6d922b85b99c77f23bf5ca96c8c7b9f3a1
Daniel Dietrich <cafebab3@gmail.com>
2022-10-06 07:59:08 +0000

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -6,32 +6,3 @@
 
 export { eager, inject } from './inject';
 export { Container, Factory, Module } from './types';
-
-import { inject } from './inject'
-
-type Container = {
-    group: {
-        service: (ctr: Container) => Service
-    }
-}
-
-// A service is a dependency, it may be any JS value
-class Service {
-    constructor(ctr: Container) {}
-}
-
-// A factory creates a dependency
-const factory = (ctr: Container) => new Service(ctr);
-
-// A module contains nested groups (optional) and service factories
-const module = {
-    group: {
-        service: factory
-    }
-}
-
-// Inject turns a module into a container
-const container = inject(module);
-
-// Services can be obtained from the container
-const service = container.group.service;
